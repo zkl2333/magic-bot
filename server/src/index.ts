20f4ee54e3d222425f5d7f9ea02fa3dea6306739
zkl2333 <i@zkl2333.com>
2023-05-07 14:51:04 +0800

    feat: 🎸 服务重启优化

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,9 +1,52 @@
 import app from './app'
+import findProcess from 'find-process'
+import { exec } from 'child_process'
+import os from 'os'
 
-// 杀死其他占用 3001 端口的进程
-import fkill from 'fkill'
-fkill(':3001', { force: true })
+const port = 3001
 
-app.listen(3001, () => {
-  console.log('Server is running on http://localhost:3001')
-})
+// 杀死占用端口的进程
+function killProcessOnPort(port: number) {
+  return new Promise<void>((resolve, reject) => {
+    findProcess('port', port)
+      .then(processList => {
+        if (processList.length === 0) {
+          console.log(`No process found on port ${port}.`)
+          resolve()
+        }
+
+        processList.forEach(process => {
+          console.log(`Killing process with PID ${process.pid} on port ${port}...`)
+          const killCommand =
+            os.platform() === 'win32' ? `taskkill /F /PID ${process.pid}` : `kill ${process.pid}`
+          exec(killCommand, (err, stdout, stderr) => {
+            if (err) {
+              console.error(`Error: ${err}`)
+              reject(err)
+            }
+            console.log(`Process with PID ${process.pid} killed successfully.`)
+            resolve()
+          })
+        })
+      })
+      .catch(err => {
+        console.error(err)
+        reject(err)
+      })
+  })
+}
+
+// 启动 Koa 服务器
+async function startKoa() {
+  try {
+    await killProcessOnPort(port)
+  } catch (err) {
+    console.error('Failed to kill process on port.')
+  }
+
+  app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`)
+  })
+}
+
+startKoa()
