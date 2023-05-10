aae9bd23896a2937e7b88c06341c9841dbab1b5f
zkl <i@zkl2333.com>
2023-05-10 18:16:47 +0800

    feat: 🎸 修改接口

--- server/src/service/openai/completions.ts
+++ server/src/service/openai/completions.ts
@@ -6,10 +6,10 @@ const createMessage = (chatList: ChatListItem[]) => {
 
 async function completions(chatList: ChatListItem[]) {
   // fetch 在 Nodejs 18 里已经可用
-  const response = await fetch(`https://key-rental-api.bowen.cool/openai/v1/chat/completions`, {
+  const response = await fetch(`http://api.aiproxy.io/v1/chat/completions`, {
     headers: {
       'Content-Type': 'application/json',
-      Authorization: `Bearer df73e83d-d0e0-401b-9cd5-965e97c52e5d`
+      Authorization: `Bearer ap-iVtcld0cTkOSiA8ke2iea4DFEFCK1jLfFXbUqLuSYsph4Zn6`
     },
     method: 'POST',
     body: JSON.stringify({
