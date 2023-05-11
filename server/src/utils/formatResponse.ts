666b0ebd10eeaff587b3ec068ee289c8a004af65
zkl <i@zkl2333.com>
2023-05-11 15:11:30 +0800

    feat: 🎸 文案优化

--- server/src/utils/formatResponse.ts
+++ server/src/utils/formatResponse.ts
@@ -1,5 +1,5 @@
 function formatChatErrorResponse(msg: object | string) {
-  const tips = `## 哎呀！服务器坏掉啦！\n1. 请稍后尝试重新加载页面，别担心，您的对交互记录没有丢失。\n2. 如问题持续存在，请及时联系管理员。\n`
+  const tips = `## 哎呀！好像坏掉啦！\n1. 请稍后点击气泡下方的重试按钮。\n2. 请稍后尝试重新加载页面，别担心，您的对交互记录没有丢失。\n3. 请尝试提供以下信息以便管理员快速定位问题：\n`
   if (typeof msg === 'string') {
     try {
       return [tips, '```json\n', JSON.stringify(JSON.parse(msg), null, 2), '\n```'].join('')
