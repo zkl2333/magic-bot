e0fd9ee269af7e66b89a5e937868884a467eedaa
zkl <i@zkl2333.com>
2023-05-15 09:37:04 +0800

    feat: 🎸 pwa

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,12 +1,37 @@
 import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd())
 
   return {
-    plugins: [react()],
+    plugins: [
+      VitePWA({
+        registerType: 'autoUpdate',
+        injectRegister: 'auto',
+        manifest: {
+          name: 'AI Web',
+          short_name: 'AI Web',
+          description: 'AI Web',
+          icons: [
+            {
+              src: '/robot_160.png',
+              sizes: '160x160',
+              type: 'image/png'
+            }
+          ]
+        },
+        devOptions: {
+          enabled: true
+        },
+        workbox: {
+          globPatterns: ['**/*.{js,css,html,ico,png,svg}']
+        }
+      }),
+      react()
+    ],
     server: {
       host: '0.0.0.0',
       proxy: {
