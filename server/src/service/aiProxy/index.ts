be743c0988a15c23002d9c0589cf6bff61a0e9cc
zkl <i@zkl2333.com>
2023-05-23 17:19:59 +0800

    feat: 🎸 自动创建ai密钥关联

--- server/src/service/aiProxy/index.ts
+++ server/src/service/aiProxy/index.ts
@@ -3,39 +3,71 @@ import { PrismaClient } from '@prisma/client'
 const sessionId = 'MTM5NWQzZGQtMTk3ZC00MjEwLWI3ZjYtNzU0NjFhZmVlZWY3'
 const prisma = new PrismaClient()
 
-export const createApiKey = async (uid: number) => {
-  const url = 'https://aiproxy.io/api/user/createApiKey'
+const baseUrl = 'https://aiproxy.io/api'
 
+const api = {
+  createApiKey: '/user/createApiKey',
+  getPointAccount: '/point/getPointAccount'
+}
+
+const defaultModelPermission = [
+  'gpt-3.5-turbo',
+  'gpt-3.5-turbo-0301',
+  'gpt-4',
+  'gpt-4-0314',
+  'gpt-4-32k',
+  'gpt-4-32k-0314',
+  'text-davinci-003',
+  'text-davinci-002',
+  'text-curie-001',
+  'text-babbage-001',
+  'text-ada-001',
+  'text-embedding-ada-002',
+  'text-search-ada-doc-001',
+  'whisper-1',
+  'text-davinci-edit-001',
+  'code-davinci-edit-001'
+]
+
+const createUrl = (path: string) => {
+  return `${baseUrl}${path}`
+}
+
+export const getPointAccount = async (uid: string) => {
+  const headers = {
+    'content-type': 'application/json',
+    cookie: `sessionId=${sessionId}`
+  }
+
+  const response = await fetch(createUrl(`${api.getPointAccount}?externalId=${uid}`), {
+    method: 'GET',
+    headers
+  })
+
+  if (!response.ok) {
+    throw new Error(`Request failed with status code ${response.status}`)
+  }
+
+  const responseBody = await response.json()
+  console.log(responseBody)
+  return responseBody.data
+}
+
+export const createApiKey = async (uid: string) => {
   const headers = {
     'content-type': 'application/json',
     cookie: `sessionId=${sessionId}`
   }
 
   const data = {
-    name: `web-user-${uid}`,
+    name: `web-user-${uid.slice(0, 8)}`,
+    externalId: uid,
     enableSubPointAccount: true,
     initPoint: '1000.00',
-    modelPermission: [
-      'gpt-3.5-turbo',
-      'gpt-3.5-turbo-0301',
-      'gpt-4',
-      'gpt-4-0314',
-      'gpt-4-32k',
-      'gpt-4-32k-0314',
-      'text-davinci-003',
-      'text-davinci-002',
-      'text-curie-001',
-      'text-babbage-001',
-      'text-ada-001',
-      'text-embedding-ada-002',
-      'text-search-ada-doc-001',
-      'whisper-1',
-      'text-davinci-edit-001',
-      'code-davinci-edit-001'
-    ]
+    modelPermission: defaultModelPermission
   }
 
-  const response = await fetch(url, {
+  const response = await fetch(createUrl(api.createApiKey), {
     method: 'POST',
     headers,
     body: JSON.stringify(data)
@@ -50,7 +82,7 @@ export const createApiKey = async (uid: number) => {
   return responseBody.data
 }
 
-export const getApiKey = async (uid: number) => {
+export const getApiKey = async (uid: string) => {
   // 判断是否存在AI密钥
   const existingUserPlatform = await prisma.userPlatform.findFirst({
     where: {
@@ -64,6 +96,7 @@ export const getApiKey = async (uid: number) => {
   }
 
   const apiKey = await createApiKey(uid)
+  console.log('创建AI密钥', apiKey.slice(0, 8))
 
   const createdUserPlatform = await prisma.userPlatform.create({
     data: {
@@ -74,6 +107,8 @@ export const getApiKey = async (uid: number) => {
       }
     }
   })
+
+  console.log('AI密钥关联成功', createdUserPlatform.apiKey.slice(0, 8))
 
   return createdUserPlatform.apiKey
 }
